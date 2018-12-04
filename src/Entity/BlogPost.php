2bf4794179bce9e6f198083e5d42f35741adb28a
apokhodun <apokhodun@determine.com>
2018-12-04 21:43:09 +0200

    Update BlogPost Entity

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,10 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiResource()
  */
 class BlogPost
 {
@@ -32,64 +36,67 @@ class BlogPost
     private $content;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="post")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $author;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="blogPost")
+     */
+    private $comment;
+
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $slug;
 
-    public function getId(): ?int
-    {
+    /**
+     * BlogPost constructor.
+     */
+    public function __construct() {
+        $this->comment = new ArrayCollection();
+    }
+
+    public function getComment(): Collection {
+        return $this->comment;
+    }
+
+    public function setComment($comment): void {
+        $this->comment = $comment;
+    }
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getTitle(): ?string
-    {
+    public function getTitle(): ?string {
         return $this->title;
     }
 
-    public function setTitle(string $title): self
-    {
+    public function setTitle(string $title): self {
         $this->title = $title;
 
         return $this;
     }
 
-    public function getPublished(): ?\DateTimeInterface
-    {
+    public function getPublished(): ?\DateTimeInterface {
         return $this->published;
     }
 
-    public function setPublished(\DateTimeInterface $published): self
-    {
+    public function setPublished(\DateTimeInterface $published): self {
         $this->published = $published;
 
         return $this;
     }
 
-    public function getContent(): ?string
-    {
+    public function getContent(): ?string {
         return $this->content;
     }
 
-    public function setContent(string $content): self
-    {
+    public function setContent(string $content): self {
         $this->content = $content;
-
-        return $this;
-    }
-
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
 
         return $this;
     }
@@ -100,5 +107,19 @@ class BlogPost
 
     public function setSlug($slug): void {
         $this->slug = $slug;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     * @return BlogPost
+     */
+    public function setAuthor(User $author): self {
+        $this->author = $author;
+
+        return $this;
     }
 }
