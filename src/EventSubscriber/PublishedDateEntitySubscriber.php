327e0e6c2539308dbfe8fd76c0a6dda2a2c40d47
apokhodun <apokhodun@determine.com>
2018-12-08 07:56:09 +0200

    Added auto publishing date subscriber

---
+++ src/EventSubscriber/PublishedDateEntitySubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\PublishedEntityInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class PublishedDateEntitySubscriber implements EventSubscriberInterface {
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setPublishedDate', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function setPublishedDate(GetResponseForControllerResultEvent $event)
+    {
+        $entity = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$entity instanceof PublishedEntityInterface || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $entity->setPublished(new \DateTime('now'));
+    }
+}
